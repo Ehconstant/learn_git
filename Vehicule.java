4aaa9f44a5e39a685dfbb2b7418a9237a3673534
Ehuié Manivoulé <ehuie@hotmail.fr>
2022-04-25 23:53:37 +0200

    print i for

--- Vehicule.java
+++ Vehicule.java
@@ -2,7 +2,7 @@ public class Vehicule {
     public Vehicule() {
         System.out.println("Vehicule !");
         for (int i = 0; i < 5; i++) {
-
+            System.out.println(i);
         }  
         
     }
